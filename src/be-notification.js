9ab026afbfadd62ef976f73b19a3f808a39e1006
Bernan Ribeiro <alves.bernan@gmail.com>
2022-10-11 16:11:50 -0300

    fixing object parameter bug

--- src/be-notification.js
+++ src/be-notification.js
@@ -1,11 +1,12 @@
 
 import { events } from "./events"
 
-export const benotification = (notification, setting) => {
+export const benotification = (notification, setting={}) => {
     if (typeof notification === "string")
         setting.message = notification    
     else 
         setting = notification
 
+    /** emit events './BeNotification.vue' */
     events.emit('notify', setting)
 }
